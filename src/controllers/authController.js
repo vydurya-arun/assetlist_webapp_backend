ac535447135e64b3750d0268972d81a1940d7a3b
ARUN KUMAR.A <arun@vydurya.in>
2025-10-30 17:49:53 +0530

    commit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -41,3 +41,16 @@ export const registerAdmin = async (req, res) => {
         return res.status(500).json({ success: false, message: error.message || "Server Error" });
     }
 }
+
+export const getAlluser = async(req,res) =>{
+    try {
+        const user = await userModel.find().select("-password");
+        if(!user){
+            return res.status(404).json({ success: false, message: "Cannot get userdata" });
+        }
+
+        return res.status(200).json({success: true, data: user})
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message || "Server Error" });
+    }
+}
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registerAdmin } from "../controllers/authController.js";
+import { getAlluser, registerAdmin } from "../controllers/authController.js";
 
 const authRouter = Router();
 
 authRouter.post('/register', registerAdmin);
+authRouter.get('/alluser', getAlluser);
 
 export default authRouter;
