f119a5a36802a7e9c6d202ffe0b19af0c957f8d6
ARUN KUMAR.A <arun@vydurya.in>
2025-10-30 10:10:37 +0530

    commit

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+import dotenv from 'dotenv';
 import express from 'express';
 
-
+dotenv.config();
 const app = express();
-const PORT = 4000;
-app.get('/',(req, res)=> res.send("Welcome to Assetlist webapp backend"));
+const PORT = process.env.PORT || 4000;
+app.get('/',(req, res)=> res.send("Welcome to Assetlist webapp backend ✅"));
 
-app.listen(PORT,()=>console.log(`server is running sucessfully on ${PORT} `))
+app.listen(PORT,()=>console.log(`server is running sucessfully on ${PORT} ✅`))
