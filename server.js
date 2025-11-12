946fc6bdd5bef56f8f9bd7eeafebd1c48deb441d
ARUN KUMAR.A <arun@vydurya.in>
2025-11-12 11:46:18 +0530

    commit

--- server.js
+++ server.js
@@ -16,9 +16,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true,             
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -6,13 +6,16 @@ import { paginate } from "../middileware/pagination.js";
 const authRouter = Router();
 
 authRouter.post('/adminRegister', registerAdmin);
-authRouter.get('/alluser',authMiddleware,paginate("user", "all_users"), getAlluser);
+authRouter.get('/allusers',authMiddleware,paginate("user", "all_users"), getAlluser);
 authRouter.post('/adminLogin', loginAdmin);
 authRouter.post('/logout', logout);
 authRouter.post('/refresh', refresh);
 authRouter.post('/createUser',authMiddleware, createUsers);
 authRouter.delete('/deleteByid/:id',authMiddleware, deleteUserById);
 authRouter.delete('/deleteAll',authMiddleware, deleteAllUser);
+authRouter.get("/profile", authMiddleware, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 
 
 export default authRouter;
